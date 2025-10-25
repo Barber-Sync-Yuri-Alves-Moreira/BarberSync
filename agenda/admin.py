d2f37afbb0a6bdf280ecd834c593b3860e2d7006
moreirayurieada <139239612+moreirayurieada@users.noreply.github.com>
2025-10-25 19:25:01 -0300

    v.5

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,25 +1,18 @@
 
-
 from django.contrib import admin
-from .models import Barbearias, Barbeiros, Servicos, BarbeirosServicos, HorariosDisponiveis 
-
-class BarbeariasAdmin(admin.ModelAdmin):
-    exclude = ('senha_hash',) 
-
-
-    actions = None 
-
-
+from .models import Barbeiros, Servicos, BarbeirosServicos, HorariosDisponiveis 
 class BarbeirosAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'id_barbearia', 'imagem')
-    list_filter = ('id_barbearia',)
-    actions = None
 
+    list_display = ('nome', 'imagem', 'descricao') 
+    search_fields = ('nome',) 
+
+class ServicosAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'duracao_minutos', 'preco')
+    search_fields = ('nome',)
+    list_filter = ('duracao_minutos', 'preco') 
 class BarbeirosServicosAdmin(admin.ModelAdmin):
     list_display = ('get_barbeiro_nome', 'get_servico_nome') 
-    list_filter = ('id_barbeiro', 'id_servico')
-    actions = None 
-
+    list_filter = ('id_barbeiro', 'id_servico') 
     def get_barbeiro_nome(self, obj):
         return obj.id_barbeiro.nome
     get_barbeiro_nome.short_description = 'Barbeiro' 
@@ -28,14 +21,23 @@ class BarbeirosServicosAdmin(admin.ModelAdmin):
         return obj.id_servico.nome
     get_servico_nome.short_description = 'Serviço' 
 
+
 class HorariosDisponiveisAdmin(admin.ModelAdmin):
-    list_display = ('id_barbeiro', 'data_hora')
-    list_filter = ('id_barbeiro', 'data_hora')
-    actions = None 
+    list_display = ('get_barbeiro_nome', 'data_hora')
+    list_filter = ('id_barbeiro', 'data_hora') 
 
 
-admin.site.register(Barbearias, BarbeariasAdmin)
+    def get_barbeiro_nome(self, obj):
+        return obj.id_barbeiro.nome
+    get_barbeiro_nome.short_description = 'Barbeiro'
+
+
 admin.site.register(Barbeiros, BarbeirosAdmin)
-admin.site.register(Servicos) 
-admin.site.register(BarbeirosServicos, BarbeirosServicosAdmin) 
+admin.site.register(Servicos, ServicosAdmin)
+admin.site.register(BarbeirosServicos, BarbeirosServicosAdmin)
 admin.site.register(HorariosDisponiveis, HorariosDisponiveisAdmin)
+
+
+admin.site.site_header = "Painel Cleber Barbearia"
+admin.site.site_title = "Admin Cleber Barbearia"
+admin.site.index_title = "Gerenciamento"
--- agenda/models.py
+++ agenda/models.py
@@ -1,98 +1,54 @@
-
-
 from django.db import models
 
-class Barbearias(models.Model):
-    nome_negocio = models.CharField(max_length=100)
-    email = models.CharField(unique=True, max_length=100)
-    senha_hash = models.CharField(max_length=255)
-    whatsapp_negocio = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.nome_negocio
-
-    class Meta:
-        db_table = 'barbearias'
-        verbose_name_plural = "Barbearias"
-
-
 class Barbeiros(models.Model):
-    id_barbearia = models.ForeignKey(
-        Barbearias, 
-        on_delete=models.CASCADE, 
-        db_column='id_barbearia',
-        verbose_name="Barbearias"
-    )
     nome = models.CharField(max_length=100)
     imagem = models.ImageField(upload_to='barbeiros/', blank=True, null=True)
     descricao = models.TextField(blank=True, null=True)
-
+    
     def __str__(self):
         return self.nome
-
+        
     class Meta:
-        db_table = 'barbeiros'
+        verbose_name = "Barbeiro"
         verbose_name_plural = "Barbeiros"
 
-
 class Servicos(models.Model):
-    id_barbearia = models.ForeignKey(
-        Barbearias, 
-        on_delete=models.CASCADE, 
-        db_column='id_barbearia',
-        verbose_name="Barbearia"
-    )
     nome = models.CharField(max_length=100)
-    duracao_minutos = models.IntegerField(blank=True, null=True)
-    preco = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-
+    duracao_minutos = models.IntegerField(help_text="Duração do serviço em minutos")
+    preco = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    
     def __str__(self):
         return self.nome
-
+        
     class Meta:
-        db_table = 'servicos'
+        verbose_name = "Serviço"
         verbose_name_plural = "Serviços"
 
-
 class BarbeirosServicos(models.Model):
-    id_barbeiro = models.ForeignKey(
-        Barbeiros, 
-        on_delete=models.CASCADE, 
-        db_column='id_barbeiro',
-        verbose_name="Barbeiro"
-    )
-    id_servico = models.ForeignKey(
-        Servicos, 
-        on_delete=models.CASCADE, 
-        db_column='id_servico',
-        verbose_name="Serviço"
-    )
-
-
+    id_barbeiro = models.ForeignKey(Barbeiros, on_delete=models.CASCADE, verbose_name="Barbeiro")
+    id_servico = models.ForeignKey(Servicos, on_delete=models.CASCADE, verbose_name="Serviço")
+    
     def __str__(self):
-
-        return f"{self.id_servico.nome} - {self.id_barbeiro.nome}"
-
-
+        barbeiro_nome = self.id_barbeiro.nome if self.id_barbeiro else 'ID ' + str(self.id_barbeiro_id)
+        servico_nome = self.id_servico.nome if self.id_servico else 'ID ' + str(self.id_servico_id)
+        return f"{barbeiro_nome} - {servico_nome}"
+        
     class Meta:
-        db_table = 'barbeiros_servicos'
-        unique_together = (('id_barbeiro', 'id_servico'),)
-        verbose_name_plural = "Relação Barbeiro-Serviço"
-
+        verbose_name = "Serviço do Barbeiro"
+        verbose_name_plural = "Serviços dos Barbeiros"
+        unique_together = ('id_barbeiro', 'id_servico')
 
 class HorariosDisponiveis(models.Model):
-    id_barbeiro = models.ForeignKey(
-        Barbeiros, 
-        on_delete=models.CASCADE, 
-        db_column='id_barbeiro',
-        verbose_name="Barbeiro"
-    )
-    data_hora = models.DateTimeField()
-
+    id_barbeiro = models.ForeignKey(Barbeiros, on_delete=models.CASCADE, verbose_name="Barbeiro")
+    data_hora = models.DateTimeField(verbose_name="Data e Hora")
+    
     def __str__(self):
-        return f"{self.id_barbeiro.nome} - {self.data_hora.strftime('%d/%m/%Y %H:%M')}"
-
+        barbeiro_nome = self.id_barbeiro.nome if self.id_barbeiro else 'ID ' + str(self.id_barbeiro_id)
+        data_hora_formatada = self.data_hora.strftime('%d/%m/%Y %H:%M') if self.data_hora else 'Data/Hora inválida'
+        return f"{barbeiro_nome} - {data_hora_formatada}"
+        
     class Meta:
-        db_table = 'horarios_disponiveis'
-        unique_together = (('id_barbeiro', 'data_hora'),)
+        verbose_name = "Horário Disponível"
         verbose_name_plural = "Horários Disponíveis"
+        unique_together = ('id_barbeiro', 'data_hora')
+        ordering = ['id_barbeiro', 'data_hora'] 
