3ae07651166cfdc6353cfd9f4114c4e83da9c05f
moreirayurieada <139239612+moreirayurieada@users.noreply.github.com>
2025-10-24 23:04:23 -0300

    Att v.4

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,12 +1,12 @@
-
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    #
-    path('', views.lista_barbeiros, name='lista_barbeiros'),
 
-    
+    path('', views.lista_barbeiros, name='lista_barbeiros'),
     path('servicos/<int:barbeiro_id>/', views.lista_servicos, name='lista_servicos'),
+    path('agendar/<int:barbeiro_id>/<int:servico_id>/', 
+         views.selecionar_data, name='selecionar_data'),
+    path('agendar/<int:barbeiro_id>/<int:servico_id>/<str:data>/', 
+         views.selecionar_horario, name='selecionar_horario'),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -1,35 +1,67 @@
-
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Barbeiros, Servicos, BarbeirosServicos, HorariosDisponiveis
-
-
+from django.utils import timezone
+from django.db.models.functions import TruncDate 
+from datetime import datetime 
 
 def lista_barbeiros(request):
+    barbeiros = Barbeiros.objects.all()
+    context = {'barbeiros': barbeiros}
+    return render(request, 'agenda/lista_barbeiros.html', context)
 
-    todos_os_barbeiros = Barbeiros.objects.all()
-
-
-    contexto = {
-        'barbeiros': todos_os_barbeiros
-    }
-    return render(request, 'agenda/lista_barbeiros.html', contexto)
-
-
-
-#
 def lista_servicos(request, barbeiro_id):
-
-    barbeiro_selecionado = Barbeiros.objects.get(id=barbeiro_id)
-
-
-    relacoes = BarbeirosServicos.objects.filter(id_barbeiro=barbeiro_selecionado)
-
-   
-    servicos_do_barbeiro = [relacao.id_servico for relacao in relacoes]
-
-    contexto = {
-        'barbeiro': barbeiro_selecionado,
-        'servicos': servicos_do_barbeiro
+    barbeiro = get_object_or_404(Barbeiros, id=barbeiro_id)
+    servicos_ids = BarbeirosServicos.objects.filter(id_barbeiro=barbeiro).values_list('id_servico', flat=True)
+    servicos = Servicos.objects.filter(id__in=servicos_ids)
+    
+    context = {
+        'barbeiro': barbeiro,
+        'servicos': servicos
     }
-    return render(request, 'agenda/lista_servicos.html', contexto)
+    return render(request, 'agenda/lista_servicos.html', context)
+
+def selecionar_data(request, barbeiro_id, servico_id):
+    barbeiro = get_object_or_404(Barbeiros, id=barbeiro_id)
+    servico = get_object_or_404(Servicos, id=servico_id)
+    
+    agora = timezone.now()
+    
+
+    horarios_futuros = HorariosDisponiveis.objects.filter(
+        id_barbeiro=barbeiro,
+        data_hora__gte=agora
+    )
+    
+    datas_disponiveis = horarios_futuros.annotate(
+        data=TruncDate('data_hora')
+    ).values('data').distinct().order_by('data')
+
+    context = {
+        'barbeiro': barbeiro,
+        'servico': servico,
+        'datas': datas_disponiveis 
+    }
+    return render(request, 'agenda/selecionar_data.html', context)
+
+def selecionar_horario(request, barbeiro_id, servico_id, data):
+    barbeiro = get_object_or_404(Barbeiros, id=barbeiro_id)
+    servico = get_object_or_404(Servicos, id=servico_id)
+    
+
+    data_selecionada = datetime.strptime(data, '%Y-%m-%d').date()
+    
+    agora = timezone.now()
+
+    horarios_do_dia = HorariosDisponiveis.objects.filter(
+        id_barbeiro=barbeiro,
+        data_hora__date=data_selecionada, 
+        data_hora__gte=agora 
+    ).order_by('data_hora')
+
+    context = {
+        'barbeiro': barbeiro,
+        'servico': servico,
+        'data_selecionada': data_selecionada,
+        'horarios': horarios_do_dia
+    }
+    return render(request, 'agenda/selecionar_horario.html', context)
